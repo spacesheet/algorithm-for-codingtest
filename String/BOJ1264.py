3f3d492d28ee5ef85045bd903c9b971f46641bc1
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-15 12:22:12 +0900

    BOJ1264 모음의 개수 수정

--- String/BOJ1264.py
+++ String/BOJ1264.py
@@ -1,27 +1,25 @@
-def vowel_checker(letter: str) -> bool:
-    vowel_list = ['A', 'I', 'O', 'U', 'E']
-    return letter in vowel_list
+vowel_list = ['A', 'I', 'O', 'U', 'E']
 
 def sentence_list_vowl_counter(input_list: list) -> list:
     result_list = []
     for sentence in input_list:
         result = 0
-        for letter in sentence:
-            if vowel_checker(letter):
+        for letter in sentence:  # 각 문자에 대해 순회하면서,
+            if letter in vowel_list:   # 모음 여부 체크
                 result += 1
         result_list.append(result)
     return result_list
 
 def main():
-  input_list = []
+  my_input_list = []
   while True:
-      input_sentence = input().upper()
-      if input_sentence != '#':
-          input_list.append(input_sentence)
+      my_input_sentence = input().upper()
+      if my_input_sentence != '#':
+          my_input_list.append(my_input_sentence)
       else:
           break
-  result_list = sentence_list_vowl_counter(input_list)
-  for i in result_list:
+  my_result_list = sentence_list_vowl_counter(my_input_list)
+  for i in my_result_list:
       print(i)
 
 main()
