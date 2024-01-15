b16ea38f2abdcb4c61e282d836b4f5f0f18d639b
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-15 12:23:19 +0900

    BOJ1264 모음의 개수 수정

--- String/BOJ1264.py
+++ String/BOJ1264.py
@@ -4,8 +4,8 @@ def sentence_list_vowl_counter(input_list: list) -> list:
     result_list = []
     for sentence in input_list:
         result = 0
-        for letter in sentence:  # 각 문자에 대해 순회하면서,
-            if letter in vowel_list:   # 모음 여부 체크
+        for letter in sentence:
+            if letter in vowel_list:
                 result += 1
         result_list.append(result)
     return result_list
