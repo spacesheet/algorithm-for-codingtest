4e4ed8c8b20e3b470efbf24fc8aadbe61bd3d3fe
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-16 12:01:07 +0900

    BOJ10871 X보다 작은 수

--- Implementation/BOJ10871.py
+++ Implementation/BOJ10871.py
@@ -1,10 +1,8 @@
 N, X = map(int, input().split())
 
 def main() :
-    number_list = list(map(lambda a : int(a) if int(a) < X else None, input().split()))
+    number_list = [int(a) for a in input().split() if int(a) < X]
 
-    filtered_list = [i for i in number_list if i is not None]
-
-    print(*filtered_list)
+    print(*number_list)
     
 main()
