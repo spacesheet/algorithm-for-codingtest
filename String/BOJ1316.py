ef5d086f5437f55898d4d5c1a48fa6c60a12f947
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-17 14:10:54 +0900

    BOJ1316 그룹 단어 체커

--- String/BOJ1316.py
+++ String/BOJ1316.py
@@ -1,5 +1,5 @@
 def group_word_checker(word):
-    for j in range(0, len(word)-1):
+    for j in range(0, len(word) - 1):
         if word[j] != word[j + 1] and word[j] in word[j + 1:]:
             return False
     return True
