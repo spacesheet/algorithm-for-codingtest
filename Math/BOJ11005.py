4d631fa918d9c19607be4e70a85541d880b13632
ParkSeolDev <parkseol.dev@gmail.com>
2024-01-26 13:05:47 +0900

    BOJ11005 진법 변환2

--- Math/BOJ11005.py
+++ Math/BOJ11005.py
@@ -1,14 +1,13 @@
 tmp = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-def base(n : int, b : int, text :str) -> str :
-    if n == 0 and text == "" :
-        return "0"
+def base(n : int, b : int) -> str :
     if n == 0 :
-        return text
-    return base(n // b, b, str(tmp[n % b]) + text)
+        return ""
+    else :
+        return base(n // b, b) + str(tmp[n % b])
 
 def main() :
     N, B = map(int, input().split())
-    print(base(N, B, ""))
+    print(base(N, B))
 
 main()
