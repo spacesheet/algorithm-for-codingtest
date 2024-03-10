318b4b27b54352b122ade5d70567bc42abbf9e77
ParkSeolDev <parkseol.dev@gmail.com>
2024-03-10 16:55:16 +0900

    BOJ1991 트리 순회

---
+++ Tree/BOJ1991.py
@@ -0,0 +1,31 @@
+N = int(input())
+tree = {}
+for i in range(N):
+    root, left, right = map(str, input().split())
+    tree[root] = left, right
+
+def pre_trav(center):
+    if center == '.':
+      return
+
+    print(center, end='')
+    pre_trav(tree[center][0])
+    pre_trav(tree[center][1])
+
+def in_trav(center):
+    if center != '.':
+        in_trav(tree[center][0])
+        print(center, end='')
+        in_trav(tree[center][1])
+
+def post_trav(center):
+    if center != '.':
+        post_trav(tree[center][0])
+        post_trav(tree[center][1])
+        print(center, end='')
+
+pre_trav('A')
+print()
+in_trav('A')
+print()
+post_trav('A')
