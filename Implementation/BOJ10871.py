970fcb2243c998fd5a5f612596907f80e1db9f9f
shacesheet <parkseol.dev@gmail.com>
2025-09-16 10:50:17 +0900

    init: rust algorithm

--- Implementation/BOJ10871.py
+++
@@ -1,8 +0,0 @@
-N, X = map(int, input().split())
-
-def main() :
-    number_list = [int(a) for a in input().split() if int(a) < X]
-
-    print(*number_list)
-    
-main()
--- Math/BOJ3052.py
+++
@@ -1,9 +0,0 @@
-def remains(number_list : list) -> set :
-    remain_list = [number % 42 for number in number_list]
-    return set(remain_list)
-
-def main() :
-    number_list = [int(input()) for _ in range(10)]
-    return print(len(remains(number_list)))
-
-main()
